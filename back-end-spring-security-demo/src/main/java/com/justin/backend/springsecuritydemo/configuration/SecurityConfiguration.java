8e7918c30c18d66ddfc3c475d293eb8c89abdc1d
zhangjunqiang <zhangjunqiang7183@126.com>
2023-04-14 12:56:26 +0800

    added multiple security filter chain config and moved deprecated config

--- back-end-spring-security-demo/src/main/java/com/justin/backend/springsecuritydemo/configuration/SecurityConfiguration.java
+++ back-end-spring-security-demo/src/main/java/com/justin/backend/springsecuritydemo/configuration/SecurityConfiguration.java
@@ -1,25 +1,17 @@
 package com.justin.backend.springsecuritydemo.configuration;
 
-import com.justin.backend.springsecuritydemo.test.orderannotation.OrderTest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 /**
  * security config class.
@@ -27,54 +19,58 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-  @Autowired
-  private List<OrderTest> orderTests;
 
   /**
-   * custom security filter chain.
+   * custom security filter chain for user path.
    */
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(customizer ->
-        customizer.requestMatchers("/").permitAll()
-            .requestMatchers("/user")
-            // for google login and need vm proxy options: -Dhttps.proxyHost=127.0.0.1 -Dhttps.proxyPort=10809
-            .hasAnyAuthority("ROLE_USER", "OIDC_USER") //
-//            .hasRole("USER")
-            .requestMatchers("/admin").hasRole("ADMIN")
-            .anyRequest().authenticated())
-        .csrf().disable() // for h2-console
-        .headers().frameOptions().disable() // for h2-console
-        .and()
-        .formLogin(Customizer.withDefaults()) // login with browser and Form
-        .httpBasic(Customizer.withDefaults()) // login with Insomnia/Postman and Basic Auth
-        .oauth2Login(Customizer.withDefaults()); // Login with Google - GitHub - Facebook or ...
+  @Order(100)
+  public SecurityFilterChain securityFilterChainForUser(HttpSecurity http) throws Exception {
+    http.securityMatcher("/user")
+        .authorizeHttpRequests(authConfig ->
+            authConfig.requestMatchers("/user/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                .anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults());
     return http.build();
   }
 
   /**
-   * utilize the embedded data source.
+   * custom security filter chain for admin path.
    */
-//  @Bean
-  public DataSource embeddedH2DataSource() {
-    orderTests.forEach(orderTest -> System.out.println(orderTest.getBeanName())); // for @Order test
-    return new EmbeddedDatabaseBuilder()
-            .setType(EmbeddedDatabaseType.H2)
-            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-            .build();
+  @Bean
+  @Order(101)
+  public SecurityFilterChain securityFilterChainForAdmin(HttpSecurity http) throws Exception {
+    http.securityMatcher("/admin")
+        .authorizeHttpRequests(authConfig ->
+            authConfig.requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+                .anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults());
+    return http.build();
   }
 
   /**
-   * custom data source, need provide the schema.sql
+   * custom security filter chain for home path.
    */
-//  @Bean
-  public DataSource customMemoryH2DataSource() {
-    return DataSourceBuilder.create()
-            .driverClassName("org.h2.Driver")
-            .url("jdbc:h2:mem:testdb")
-            .username("sa")
-            .password("")
-            .build();
+  @Bean
+  @Order(102)
+  public SecurityFilterChain securityFilterChainForHome(HttpSecurity http) throws Exception {
+    http.securityMatcher("/")
+        .authorizeHttpRequests(authConfig ->
+            authConfig.anyRequest().permitAll())
+        .formLogin(Customizer.withDefaults());
+    return http.build();
+  }
+
+  /**
+   * custom security filter chain for other path.
+   */
+  @Bean
+  @Order(103)
+  public SecurityFilterChain securityFilterChainForOther(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authConfig ->
+            authConfig.anyRequest().denyAll())
+        .formLogin(Customizer.withDefaults());
+    return http.build();
   }
 
   @Bean
@@ -86,24 +82,5 @@ public class SecurityConfiguration {
   @Bean
   public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
     return new JdbcUserDetailsManager(dataSource);
-  }
-
-  /**
-   * in memory user detail config
-   * ConditionalOnMissingBean is base on the order of bean definition in the class.
-   */
-//  @Bean
-  public UserDetailsService userDetailsService() {
-    var admin = User.builder()
-            .username("admin")
-            .password("{noop}admin")
-            .roles("ADMIN", "USER")
-            .build();
-    var user = User.builder()
-            .username("justin")
-            .password("{noop}justin")
-            .roles("USER")
-            .build();
-    return new InMemoryUserDetailsManager(admin, user);
   }
 }

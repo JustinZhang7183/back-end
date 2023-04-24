9ad42223c9c40f1f36321cc9d0d7a562eb8c5c30
zhangjunqiang <zhangjunqiang7183@126.com>
2023-04-24 16:21:49 +0800

    implementation of service discovery by zookeeper

--- back-end-spring-cloud-zookeeper/src/main/java/com/justin/backend/springcloudzookeeper/controller/DemoController.java
+++ back-end-spring-cloud-zookeeper/src/main/java/com/justin/backend/springcloudzookeeper/controller/DemoController.java
@@ -2,10 +2,14 @@ package com.justin.backend.springcloudzookeeper.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RefreshScope
@@ -20,6 +24,9 @@ public class DemoController {
   @Value("${mysql-password}")
   private String mysqlPassword;
 
+  @Resource
+  private DiscoveryClient discoveryClient;
+
   @RequestMapping("/value")
   public String value() {
     return "ip: " + ip + "\n" + "mysqlPassword: " + mysqlPassword;
@@ -28,5 +35,10 @@ public class DemoController {
   @RequestMapping("/env")
   public String env() {
     return "ip: " + environment.getProperty("ip") + "\n" + "mysqlPassword: " + environment.getProperty("mysql-password");
+  }
+
+  @RequestMapping("/discovery")
+  public List<ServiceInstance> serviceUrl() {
+    return discoveryClient.getInstances("spring-cloud-zookeeper");
   }
 }
